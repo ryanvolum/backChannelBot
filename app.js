6bf0174392262014482720212d1948da1e0c27c8
ryvolum <ryvolum@microsoft.com>
2017-01-26 14:01:33 -0800

    removed LUIS integration - made app.js simpler

--- app.js
+++ app.js
@@ -1,102 +1,31 @@
 require('./connectorSetup.js')();
-require('./callLUIS.js')();
 
-const modelUrl = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9a9b25d8-4cb1-4caf-949b-dadcba63922e?subscription-key=1f4ea5b6343241678394dc5f47bfed30";
-// Entry point of the bot
+//Bot listening for inbound backchannel events - in this case it only listens for events named "buttonClicked"
+bot.on("event", function (event) {
+    var msg = new builder.Message().address(event.address);
+    msg.data.textLocale = "en-us";
+    if (event.name === "buttonClicked") {
+        msg.data.text = "I see that you just pushed that button";
+    }
+    bot.send(msg);
+})
+
+//Basic dialog which takes an inputted color sends a changeBackground event. No NLP, regex, validation here - just grabs input and sends it back as an event. 
 bot.dialog('/', [
     function (session) {
-        var msg = session.message.text;
-        recognizeThis(msg, modelUrl, function (err, results, entities) {
-            var s = "";
-            if (results && results.intent && results.intent != "None") {
-                session.beginDialog("/" + results.intent, { entities });
-            } else {
-                session.send("Really not sure what you're going for there, champ.");
-            }
-        })
+        var reply = createEvent("changeBackground", session.message.text, session.message.address);
+        session.endDialog(reply);
     }
 ]);
 
-bot.dialog('/changeBackground', [
-    function (session, args) {
-        if (args && args.entities && args.entities[0]){
-            /*
-            var msg = {
-                "type": "event",
-                "text": "You wanted to change the background to " + args.entities[0].entity,
-                "from": {
-                    "id": session.message.address.bot.id,
-                    "name": "Bot"
-                },
-                "recipient": {
-                    "id": session.message.user.id
-                },
-                "channelId": session.message.address.bot.channelId,
-                "conversation": {
-                    "id": session.message.address.conversation.id
-                }
-            }
-            */
-        }
-        session.endDialog("You wanted to change your background to " + args.entities[0].entity);
-    }
-]);
+//Creates a backchannel event
+const createEvent = (eventName, value, address) => {
+    var msg = new builder.Message().address(address);
+    msg.data.type = "event";
+    msg.data.name = eventName;
+    msg.data.value = value;
+    return msg;
+}
 
-bot.dialog('/error', [
-    function (session) {
-        var msg = "Seems I slipped up - sorry!";
-        session.send(msg);
-        global.restartDialog(session, '/promptButtons');
-    }
-]);
-
-bot.dialog('/socialMedia', [
-    function (session) {
-        var msg = "Tweet about the event at https://twitter.com/AIWorldExpo !"
-        session.send(msg);
-        global.restartDialog(session, '/promptButtons');
-
-    }
-]);
-
-bot.dialog('/promptButtons', [
-    function (session) {
-        var choices = [
-            "Schedule Explorer",
-            "Sponsors/Expos",
-            "Speaker Search",
-            "Social Media"]
-
-        if (session.message.source === "skype") {
-            builder.Prompts.choice(session, "How would you like to explore AI World?", choices, { listStyle: builder.ListStyle.button });
-        } else {
-            builder.Prompts.choice(session, "How would you like to explore AI World?", choices);
-        }
-    },
-    function (session, results) {
-        if (results.response) {
-            session.privateConversationData.clickingButtons = true;
-            var selection = results.response.entity;
-            // route to corresponding dialogs
-            switch (selection) {
-                case "Schedule Explorer":
-                    session.replaceDialog('/sessions');
-                    break;
-                case "Speaker Search":
-                    session.replaceDialog('/people');
-                    break;
-                case "Sponsors/Expos":
-                    session.replaceDialog('/sponsors');
-                    break;
-                case "Social Media":
-                    session.replaceDialog('/socialMedia');
-                    break;
-                default:
-                    restart(session);
-                    break;
-            }
-        }
-    }
-]);
 
 
